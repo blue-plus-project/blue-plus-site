58e8c8d4469a850e85a419abbe5157bd7cffbcb4
Victor <victubou@gmail.com>
2024-06-05 20:50:54 -0300

    Criando o sistema de cadastro de e-mail

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 let imagens=['/src/assets/sensores/eletrodo de pH.jpg', '/src/assets/sensores/sensor baseado em luz dispersa.jpg', '/src/assets/sensores/sensor de condutividade elétrica.jpg', '/src/assets/sensores/sensor de metais pesados.jpg', '/src/assets/sensores/sensor de temperatura digital.jpg', '/src/assets/sensores/sensor optico.jpg'];
 let index = 0;
-let time = 3000;
+let time = 1500;
 
 function slideShow(){
     document.getElementById('sensores').src = imagens[index];
@@ -12,3 +12,21 @@ function slideShow(){
     setTimeout('slideShow()', time);
 }
 slideShow();
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    let email = document.querySelector(".info-email");
+    let msg = document.querySelector(".mensagem");
+
+    msg.classList.add("hidden");
+
+    email.addEventListener("keyup", function(event){
+        if(event.keyCode === 13) {
+            event.preventDefault();
+            msg.classList.remove("hidden")
+            email.style.display="none";
+            email.value = "";
+            alert("Seu e-mail foi cadastrado com sucesso.");
+        }
+    })
+})
